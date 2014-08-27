db42acc03dca011129dea267a84856d8a19d4d25
Rahul Raina <rahulr0609@hotmail.com>
2014-08-26 23:50:37 -0400

    Cleaned up base css and base js files to keep only needed necessary files

--- mealjet/apps/core/static/core/js/script.base.js
+++ mealjet/apps/core/static/core/js/script.base.js
@@ -2,65 +2,10 @@
 
 $(document).ready(function(){
 
-$('#accord-one').show();
-
-
-
-  $('#accordion').on('show.bs.collapse', function () {
-      $('#accordion .in').collapse('hide');
-  });
-
-$('.accordion-toggle').on('click',function(e){
-    if($(this).parents('.panel').children('.panel-collapse').hasClass('in')){
-        e.stopPropagation();
-    }
-});
-
 
 
 
 }); //END OF DOCUMENT READY
 
 
-
-
-function addExpTitle() {
-
-    var elt = $('#cc_exp_title');
-    elt.removeClass('has-success has-error');
-
-    if(!paymentFormFlags['cc_exp_year'] &&
-        !paymentFormFlags['cc_exp_month'] )
-    {
-        elt.addClass('has-success');
-    }
-
-    if(paymentFormFlags['cc_exp_year'] == 1 ||
-        paymentFormFlags['cc_exp_month'] == 1)
-    {
-        elt.addClass('has-error');
-    }
-
-}
-
-function addError(elt, success, err) {
-    elt.removeClass(success + ' ' + err);
-    elt.addClass(err);
-    elt.children('.glyphicon-ok').addClass('hide');
-    elt.children('.glyphicon-remove').removeClass('hide');
-}
-
-function addSuccess(elt, success, err) {
-
-    elt.removeClass(success + ' ' + err);
-    elt.addClass(success);
-    elt.children('.glyphicon-remove').addClass('hide');
-    elt.children('.glyphicon-ok').removeClass('hide');
-}
-
-function addDefault(elt, success, err) {
-  elt.removeClass(success + ' ' + err);
-  elt.children('.glyphicon-ok').addClass('hide');
-  elt.children('.glyphicon-remove').addClass('hide');
-}
 
