738328d8da3b395e59a543cb457f24b98ba3ea34
Rahul Raina <rahulr0609@hotmail.com>
2014-08-28 10:32:34 -0400

    Updated to use generic TemplateView to remain DRY. Will be using class based views going forward

--- mealjet/apps/core/urls.py
+++ mealjet/apps/core/urls.py
@@ -1,12 +1,19 @@
 from django.conf.urls import patterns, url
 
 from apps.core import views
+from django.views.generic.base import TemplateView
 
 urlpatterns = patterns('',
     # ex: /
-    url(r'^$', views.base, name='base'),
+    url(r'^$',
+        TemplateView.as_view(template_name='core/base.html'),
+        name='base'),
     # ex: /about
-    url(r'^about/$', views.about, name='about'),
+    url(r'^about/$',
+        TemplateView.as_view(template_name='core/about.html'),
+        name='about'),
     # ex: /faq
-    url(r'^faq/$', views.faq, name='faq'),
+    url(r'^faq/$',
+        TemplateView.as_view(template_name='core/faq.html'),
+        name='faq'),
 )
--- mealjet/apps/core/views.py
+++ mealjet/apps/core/views.py
@@ -3,15 +3,3 @@ from django.shortcuts import render
 # Create your views here.
 
 #TODO: Update the base.html navigation to use templated views
-def base(request):
-    context = {}
-    return render(request, 'core/base.html', context)
-
-def about(request):
-    context = {}
-    return render(request, 'core/about.html', context)
-
-def faq(request):
-    context = {}
-    return render(request, 'core/faq.html', context)
-
