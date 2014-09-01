12ff4c452ef154abf5d5968e8cc2b0bb7565718f
adnant <adnant@gmail.com>
2014-09-01 16:35:52 -0500

    init fabric

--- fabfile.py
+++ fabfile.py
@@ -33,28 +33,131 @@ def server() :
     # your user on that system
     env.user = 'ubuntu' 
     # Assumes that your *.pem key is in the same directory as your fabfile.py
-    env.key_filename = "~/.ssh/mealJetAdmin.pem"
+    env.key_filename = '~/.ssh/mealJetAdmin.pem'
 
-def staging() :
+
+def init_directories(virtenv_name, stage):
+    
+    with cd('~/'):
+        run ('mkdir web')
+        run ('mkdir web/%s.mealjet.co' % stage)
+
+        run ('mkdir web/%s.mealjet.co/website' % stage)
+
+        run ('mkdir web/%s.mealjet.co/logs' % stage)
+
+        #Make two log files for supervisor process
+        run('touch web/%s.mealjet.co/logs/access.log' % stage)
+        run('touch web/%s.mealjet.co/logs/error.log' % stage)
+
+
+def load_dependencies(virtenv_name, stage):
+
+    sudo ('apt-get update')
+    sudo ('apt-get install python python-pip git vim')
+    sudo ('pip install virtualenv')
+
+    with cd('web/%s.mealjet.co' % stage):
+        run('virtualenv %s' % virtenv_name)
+
+
+def init_git(virtenv_name, stage):
+
+    with cd('~/web/%s.mealjet.co/website' % stage):
+        run ('git init')
+        git_ssh_path = prompt("What's the git SSH path: ")
+        run ('git remote add origin %s' % git_ssh_path)
+        run ('git pull origin %s' % stage)
+
+
+def init(virtenv_name, stage):
+
+    init_directories(virtenv_name, stage) 
+    load_dependencies(virtenv_name, stage) 
+    init_git(virtenv_name, stage)
+    install_supervisor(virtenv_name, stage)
+
+
+def beta(arg) :
+
+    """This pushes to the EC2 instance defined below"""
+    # The Elastic IP to your server
+    env.host_string = '54.68.9.120'
+    # your user on that system
+    env.user = 'ubuntu' 
+    # Assumes that your *.pem key is in the same directory as your fabfile.py
+    env.key_filename = '~/.ssh/mealJetBeta.pem'
+
+    stage = 'beta'
+
+    # ex: 'prodenv' or 'betaenv'
+    virtenv_name = stage + 'env'
+
+    if(arg == 'init'):
+        init(virtenv_name, stage)
+
+    deploy(virtenv_name, stage)
+
+
+def deploy(virtenv_name, stage) :
+
+
     # path to the directory on the server where your vhost is set up
-    path = "/home/ubuntu/web/prod.mealjet.co"
+    path = '/home/ubuntu/web/%s.mealjet.co' % stage
+    
     # name of the application process
 
     print(red("Beginning Deploy:"))
-    with cd("%s/app" % path) :
+    with cd("%s/website" % path) :
         run("pwd")
         print(green("Pulling master from GitHub..."))
-        run("git pull origin master")
+        run("git pull origin %s" % stage)
+
         print(green("Installing requirements..."))
-        run("source %s/prodenv/bin/activate && pip install -r reqs/dev.txt" % path)
+        run("source %s/%s/bin/activate && pip install -r reqs/%s.txt" % path, virtenv_name, stage)
+
         print(green("Collecting static files..."))
-        #run("source %s/prodenv/bin/activate && python manage.py collectstatic --noinput" % path)
+        #run("source %s/%s/bin/activate && python manage.py collectstatic --noinput" % path, virtenv_name, )
+
+
         print(green("Syncing the database..."))
-        run("source %s/prodenv/bin/activate && python manage.py syncdb" % path)
+        run("source %s/%s/bin/activate && python manage.py syncdb" % path, virtenv_name)
+
         print(green("Migrating the database..."))
-        run("source %s/prodenv/bin/activate && python manage.py migrate" % path)
-        restart_gunicorn()
+        run("source %s/%s/bin/activate && python manage.py migrate" % path, virtenv_name)
+
+        restart_gunicorn(virtenv_name)
+
     print(red("DONE!"))
+
+def install_supervisor(virtenv_name, stage):
+
+    # -f installs all the dependencies
+    sudo('apt-get -f install supervisor')
+
+    supervisor_file_path = '/etc/supervisor/conf.d/%s.conf' % virtenv_name
+
+    # Remove old file if it exits
+    sudo('rm -rf ' + supervisor_file_path)
+
+    #Create a new empty file
+    sudo('touch ' + supervisor_file_path)
+
+    #Build new supervisor file
+    sudo("echo '[program:%s]' >> %s" % (virtenv_name, supervisor_file_path))
+    sudo("echo 'command = /home/ubuntu/web/%s.mealjet.co/app/bin/gunicorn_start' >> %s" % (stage, supervisor_file_path))
+    sudo("echo 'user = ubuntu' >> %s" % supervisor_file_path)
+    sudo("echo 'stdout_logfile =  /home/ubuntu/web/%s.mealjet.co/logs/access.log' >> %s" % (stage, supervisor_file_path))
+    sudo("echo 'stderr_logfile =  /home/ubuntu/web/%s.mealjet.co/logs/error.log' >> %s" % (stage, supervisor_file_path))
+
+    print(red("Done creating supervisor configs!"))
+
+    #Make supervisor load new process from files
+    sudo('supervisorctl reread')
+    sudo('supervisorctl update')
+
+    print(red("Done installing supervisor!"))
+
 
 def set_env_defaults():
     env.setdefault('remote_workdir', '/home/ubuntu/web/prod.mealjet.co/app')
@@ -64,14 +167,14 @@ def set_env_defaults():
 
 
 
-def restart_gunicorn():
+def restart_gunicorn(virtenv_name):
+
     """Restart hard the Gunicorn process"""
     puts(colors.red("Stopping!"))
-    sudo ('supervisorctl stop prodenv')
+    sudo ('supervisorctl stop %s' % virtenv_name)
     puts(colors.red("Start!"))
 
-    sudo ('supervisorctl start prodenv')
-
+    sudo ('supervisorctl start %s' % virtenv_name)
 
 
 def start():
