b2ec5d223c0d83ddad5acad660d77ba82a7cfd62
Rahul Raina <rahulr0609@hotmail.com>
2014-09-06 04:39:47 -0400

    rest api fix

--- mealjet/urls.py
+++ mealjet/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from django.conf.urls import patterns, include, url
 
+from django.conf.urls import url, include
+from rest_framework import routers
+from tutorial.quickstart import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/admin/#hooking-adminsite-instances-into-your-urlconf
 admin.autodiscover()
@@ -13,6 +20,13 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
+    # Wire up our API using automatic URL routing.
+    # Additionally, we include login URLs for the browseable API.
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    
     # All other urls will be routed via core
-    url(r'', include('apps.core.urls', namespace='core')),
+    #url(r'', include('apps.core.urls', namespace='core')),
 )
+
+
